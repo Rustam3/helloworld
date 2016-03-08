1b65d51929e8dd15bd00fc5eb12a399b7c15dc05
Rustam3 <raf.rust@gmail.com>
2016-03-08 07:38:33 +0300

    Update testbot.php

--- testbot.php
+++ testbot.php
@@ -3,9 +3,9 @@
 $bottoken = "212604140:AAFjfHcqZhJrf350R0cq3GPWle9SNnUWxEo";
 $website = "https://api.telegram.org/bot".$bottoken;
 
-
+echo"hello1";
 $update = file_get_contents('php://input');
-
+echo"hello2";
 $updatearray = json_decode($update, TRUE);
 
 $length = count($updatearray["result"]);
