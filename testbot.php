1dd35b0f9e3d64059344ad5bc14db38ea779d15d
Rustam3 <raf.rust@gmail.com>
2016-03-08 07:29:51 +0300

    php

---
+++ testbot.php
@@ -0,0 +1,15 @@
+<?php
+
+$bottoken = "212604140:AAFjfHcqZhJrf350R0cq3GPWle9SNnUWxEo";
+$website = "https://api.telegram.org/bot".$bottoken;
+
+
+$update = file_get_contents('php://input');
+
+$updatearray = json_decode($update, TRUE);
+
+$length = count($updatearray["result"]);
+$chatid = $updatearray["result"][$length-1]["message"]["chat"]["id"];
+$text = $updatearray["result"][$length-1]["message"]["text"];
+
+file_get_contents($website."/sendmessage?chat_id=".$chatid."&text=".$text);
